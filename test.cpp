dab5595c69b36bab69f2bfc5b7814e3090f3311a
axf <1460014874@qq.com>
2024-10-09 12:50:35 +0800

    '重构线程池框架'

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include "ThreadPool.h"
 #include <iostream>
 #include <vector>
+std::atomic<int> sum = 0;
 // 函数：判断一个数是否是素数
 bool isPrime(int n)
 {
@@ -22,6 +23,7 @@ bool isPrime(int n)
 // 计算密集型函数：在一个范围内计算素数
 void calculatePrimes(int start, int end, int &primeCount)
 {
+    sum++;
     primeCount = 0;
     for (int i = start; i <= end; ++i)
     {
@@ -31,25 +33,61 @@ void calculatePrimes(int start, int end, int &primeCount)
         }
     }
 }
+ThreadPool t(16);
+void show()
+{
+    while (t.isRunning())
+    {
+        for (auto v : t.num)
+            std::cout << v << " ";
+        std::cout << "end\n";
+        for (auto v : t.num)
+            std::cout << v << " ";
+        std::cout << "A\n";
+        for (auto &v : t.m_tasks)
+            std::cout << v.size() << " ";
+        std::cout << "B\n";
+        std::cout << t.m_managerTasks.size() << "C\n";
+        // for (auto &v : t.m_tasks)
+        //     std::cout << v.size() << " ";
+        // std::cout << "\n";
+        std::cout << "runnuing:" << t.isRunning() << "\n";
+        // std::cout << t.m_managerTasks.size() << "end\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    for (auto v : t.num)
+        std::cout << v << " ";
+    std::cout << "A\n";
+    for (auto &v : t.m_tasks)
+        std::cout << v.size() << " ";
+    std::cout << "B\n";
+    std::cout << t.m_managerTasks.size() << "C\n";
+    std::cout << t.isRunning() << "D\n";
+}
 int main()
 {
-    ThreadPool t(8,true);
+    // std::thread Tt(show);
     auto start = std::chrono::system_clock::now();
     std::vector<std::future<void>> arr;
-    for (int i = 0; i < 500000; ++i)
+    for (int i = 0; i < 1000000; ++i)
     {
+        // arr.push_back(t.submit([]()
+        //                        { int ans  =0;
+        //       calculatePrimes(1,10000,ans); }));
         arr.push_back(t.submit([]()
-                               { int ans  =0;
-            calculatePrimes(1,10000,ans); }));
+                               { sum++; }));
     }
-    std::cout << t.getThreadNum() << "\n";
+    t.close();
     for (auto &v : arr)
         v.get();
-    t.close();
     auto end = std::chrono::system_clock::now();
     // 计算时间差并转换为毫秒
+    std::cout << "sum:" << sum << "\n";
     std::chrono::duration<double> diff = end - start;
     auto diff_in_millis = std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
     std::cout << "Time difference: " << diff_in_millis << " ms" << std::endl;
+
+    // if (Tt.joinable())
+    //     Tt.join();
     return 0;
 }
